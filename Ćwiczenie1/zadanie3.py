725782af7dd07c745c4bb37783928ae1219e4340
Lisiozmur <115721790+Lisiozmur@users.noreply.github.com>
2023-06-10 22:49:55 +0200

    Update zadanie3.py

--- Ćwiczenie1/zadanie3.py
+++ Ćwiczenie1/zadanie3.py
@@ -11,3 +11,12 @@ def zadanie3():
     print("Przewidywana Liczna = ",srednia)
     print ("Wylosowana Liczba = ", random.randint(0,10))
 zadanie3()
+
+WERSJA 2
+
+import random
+suma = 0
+for i in range(1000000):
+    liczba = random.randint(1, 10)
+    suma = (suma+liczba)/2
+print("Liczba przewidziana: ", "%.0f" % suma
